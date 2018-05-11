27c347d5313c53bfc83426756ddad9fe2298c495
Michal <mrotke@gmail.com>
2018-05-11 19:23:47 +0200

    added LCD display

--- mieszadlo/mieszadlo.ino
+++ mieszadlo/mieszadlo.ino
@@ -1,7 +1,14 @@
 #include "Arduino.h"
+#include <Wire.h>
+#include <LiquidCrystal_PCF8574.h>
+
+LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
 //The setup function is called once at startup of the sketch
 void setup()
 {
+	Wire.begin();
+	lcd.begin(16, 2); // initialize the lcd
 	pinMode(LED_BUILTIN, OUTPUT);
 	digitalWrite(LED_BUILTIN, LOW);
 // Add your initialization code here
--- mieszadlo/sloeber.ino.cpp
+++ mieszadlo/sloeber.ino.cpp
@@ -2,10 +2,12 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-05-11 19:03:15
+//This file has been generated on 2018-05-11 19:23:29
 
 #include "Arduino.h"
 #include "Arduino.h"
+#include <Wire.h>
+#include <LiquidCrystal_PCF8574.h>
 
 void setup() ;
 void loop() ;
