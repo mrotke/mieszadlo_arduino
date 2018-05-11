31fd8348625d5c30e417927df10de56d475178f5
Michal <mrotke@gmail.com>
2018-05-11 19:13:40 +0200

    changes in build script

--- mieszadlo/buildtools/script.sh
+++ mieszadlo/buildtools/script.sh
@@ -3,7 +3,7 @@ for i in {0..1}
 do
 for f in *.hex
 do
-/home/michal/Pobrane/arduino-1.8.5-linux64/arduino-1.8.5/hardware/tools/avr/bin/avrdude -C/home/michal/Pobrane/arduino-1.8.5-linux64/arduino-1.8.5/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyUSB$i -b115200 -D -Uflash:w:$f:i 
+/home/michal/Pobrane/arduino-1.8.5-linux64/arduino-1.8.5/hardware/tools/avr/bin/avrdude -C/home/michal/Pobrane/arduino-1.8.5-linux64/arduino-1.8.5/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyUSB0 -b57600 -D -Uflash:w:$f:i
 done
 done
 $SHELL
--- mieszadlo/mieszadlo.ino
+++ mieszadlo/mieszadlo.ino
@@ -2,6 +2,8 @@
 //The setup function is called once at startup of the sketch
 void setup()
 {
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, LOW);
 // Add your initialization code here
 }
 
